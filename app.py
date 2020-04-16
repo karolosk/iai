a60089757b96debb0c3b399cd23fd5c6ecdfbc55
karolosk <karolos.koutsoulelos@gmail.com>
2020-04-17 00:00:42 +0300

    Building app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import logging
 import pprint
 from datetime import datetime
 import settings
-# from service.data_service import init_db
+from service.data_service import init_db
 from service.etl_service import etl
 log = logging.getLogger(__name__)
 
-
+init_db()
 etl()
 # pprint.pprint(response.json())
 
@@ -20,5 +20,3 @@ etl()
 # print(response.json()['meta']['next'])
 # Base.metadata.drop_all(db.engine)
 
-# init_db()
-print("---------------------------------------------------------------------------------------------------------------------------------------")
